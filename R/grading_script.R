42e518d142b5364ae72cc0467788f5904fbc20c7
Jonathan Trattner <accounts@jdtrat.com>
2021-02-10 21:03:30 -0500

    test specifying token

--- R/grading_script.R
+++ R/grading_script.R
@@ -1,27 +1,37 @@
 my_repo <- Sys.getenv("MY_GITHUB_REPO")
 
-GITHUB_PAT <- Sys.getenv("GITHUB_PAT")
+path <- simplegit::check_path(path = my_repo)
 
-ds4psych_grading <- function(path) {
-
-  # If Mason is not a collaborator, send her an invite and tag her in an issue
-  if (!simplegit::gh_collab_check(path, "smasongarrison", messages = FALSE)) {
-    simplegit::gh_collab_invite(path, "smasongarrison")
-    simplegit::gh_issue_new(path, title = "Please grade me!", body = "@smasongarrison, I've finished my lab. Please grade me!")
-  } else {
-    # if Mason is a collaborator, check to see whether there are any issues that mention her.
-    # If so, assign her to all of them and remind her! If not, she must have closed them (:
-    if (simplegit::gh_issue_mention(path, collaborator = "smasongarrison")) {
-      # Assign Mason to those -- nothing happens if we reassign
-      which_issues <- as.numeric(simplegit::gh_issue_list(path, mentioned = "smasongarrison")$number)
-      lapply(which_issues, simplegit::gh_issue_assign, path = path, assignees = "smasongarrison")
-      # Add a comment pinging Mason to remind her!
-      lapply(which_issues, simplegit::gh_issue_comment, path = path,
-             body = "@smasongarrison, this is a reminder to please grade me. I will reappear every 48 hours until you close this issue.")
-    }
-  }
-
-}
+gh::gh("POST / repos/{owner}/{repo}/issues",
+       owner = path[1],
+       repo = path[2],
+       title = "Does this work?",
+       body = "I really hope so @smasongarrison. I'm almost there? Maybe?",
+       .token = Sys.getenv("GITHUB_PAT"))
 
 
-ds4psych_grading(path = my_repo)
+# ds4psych_grading <- function(path) {
+#
+#   # If Mason is not a collaborator, send her an invite and tag her in an issue
+#   if (!simplegit::gh_collab_check(path, "smasongarrison", messages = FALSE)) {
+#     simplegit::gh_collab_invite(path, "smasongarrison")
+#     simplegit::gh_issue_new(path, title = "Please grade me!", body = "@smasongarrison, I've finished my lab. Please grade me!")
+#   } else {
+#     # if Mason is a collaborator, check to see whether there are any issues that mention her.
+#     # If so, assign her to all of them and remind her! If not, she must have closed them (:
+#     if (simplegit::gh_issue_mention(path, collaborator = "smasongarrison")) {
+#       # Assign Mason to those -- nothing happens if we reassign
+#       which_issues <- as.numeric(simplegit::gh_issue_list(path, mentioned = "smasongarrison")$number)
+#       lapply(which_issues, simplegit::gh_issue_assign, path = path, assignees = "smasongarrison")
+#       # Add a comment pinging Mason to remind her!
+#       lapply(which_issues, simplegit::gh_issue_comment, path = path,
+#              body = "@smasongarrison, this is a reminder to please grade me. I will reappear every 48 hours until you close this issue.")
+#     }
+#   }
+#
+# }
+#
+#
+# ds4psych_grading(path = my_repo)
+#
+#
